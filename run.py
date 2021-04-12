bb26c3e8c5ec2fd923badbda4b0ac2cfd886f60c
Harvard-Westlake <github@hw.com>
2021-04-12 08:26:13 -0700

    Initial commit

---
+++ run.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+#This is a test program to run today in class!
+# Cntrl + Shift + P   =>  type 'run'  and select 'Run script'
+x = np.array([-1.2, 1.2])
+np.absolute(x)
+np.absolute(1.2 + 1j)
