5f386981ae631c883bdc5ac324bec23c87d6d1dd
gbishop16 <gqbishop@gmail.com>
2021-04-16 09:39:24 -0700

    installed node, squishA, closetoBoundary part 1

---
+++ squish.js
@@ -0,0 +1,21 @@
+function squishA(input)
+{
+  var output = 1 / (1 + Math.exp(-input));
+  return output;
+}
+var inputs = [-1337.1337, -10.5, -5.5, -1.5, -1, -0.3, -0.113, 0, 0.31, 0.76, 1.3, 1.99, 7.4, 160, 1337];
+console.log("squishA");
+for(var i=0;i<inputs.length;i++)
+{
+  console.log("Input: " + inputs[i] + "  Out: " + squishA(inputs[i]));
+}
+function CloseToBoundaryA(input2)
+{
+
+}
+var inputs2 = [0, 0.000027535691114583473, 0.004070137715896128, 0.18242552380635635, 0.2689414213699951, 0.425557483188341, 0.47178002201963243, 0.5, 0.5768852611320463, 0.6813537337890256, 0.7858349830425586, 0.8797431375322491, 0.9993891206405656, 1, 1];
+console.log("CloseToBoundaryA");
+for(var i =0;i<inputs2.length;i++)
+{
+  console.log("Input: " + inputs2[i] + "  Output: " + CloseToBoundaryA(inputs2[i]));
+}
---
+++ squish.py
@@ -0,0 +1,6 @@
+var inputs = [-1337.1337, -10.5, -5.5, -1.5, -1, -0.3, -0.113, 0, 0.31, 0.76, 1.3, 1.99, 7.4, 160, 1337];`
+console.log("squishA")
+for(var i=0;i<inputs.length;i++)
+{
+  console.log("Input: " + inputs[i] + " Out: " + squishA(inputs[i]));
+}
